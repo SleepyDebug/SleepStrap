c9559ce3f8f8ade56eafad55df2cda5b563f97c0
SleepyDebug <171101568+SleepyDebug@users.noreply.github.com>
2026-07-30 13:40:50 -0400

    fix font color popup startup

--- SleepStrap/UI/Elements/Settings/Pages/TexturesPage.xaml.cs
+++ SleepStrap/UI/Elements/Settings/Pages/TexturesPage.xaml.cs
@@ -12,6 +12,7 @@ namespace SleepStrap.UI.Elements.Settings.Pages
         {
             DataContext = new VisualModsViewModel();
             InitializeComponent();
+            FontColorPopup.PlacementTarget = FontColorButton;
             Loaded += (_, _) => { BuildFontColorWheel(); UpdateFontColorUi(); };
         }
 
